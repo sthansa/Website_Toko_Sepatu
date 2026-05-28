e967980ad1aa7bc94159a9e335d1c593cc61e071
Suthansa Gading Prasetyo <suthansagading13@gmail.com>
2026-05-28 20:13:15 +0700

    Delete minimarket directory

--- minimarket/includes/footer.php
+++
@@ -1,9 +0,0 @@
-        </main>
-    </div>
-    
-    <footer class="footer">
-        <p>&copy; <?php echo date('Y'); ?> Minimarket System. All rights reserved.</p>
-    </footer>
-</body>
-</html>
-
--- minimarket/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Redirect ke halaman login dengan path yang benar
-$redirect_url = 'auth/index.php';
-header("Location: " . $redirect_url);
-exit();
-?>
-
